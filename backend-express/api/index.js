8f9fd02eb8506b3a4413eb4704ca2101883744db
rohitroy@github <rhtry.tech@gmail.com>
2025-04-13 00:54:20 +0530

    update: vercel.json issue solve

---
+++ backend-express/api/index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import authRoutes from '../routes/authRoutes.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/auth', authRoutes);
+
+export default app;
+
+
+
+// ############################################################################3
+
+
+// import express from 'express';
+// import cors from 'cors';
+// import dotenv from 'dotenv';
+// import authRoutes from './routes/authRoutes.js';
+
+// dotenv.config();
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+
+// app.use('/auth', authRoutes);
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
