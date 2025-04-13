593b44e5bf21867a13ce1f354f7c37b358a357b8
rohitroy@github <rhtry.tech@gmail.com>
2025-04-14 00:53:22 +0530

    update: server changes

--- backend-express/controllers/instagramController.js
+++ backend-express/controllers/instagramController.js
@@ -1,7 +1,17 @@
 import axios from "axios";
 
 export const redirectToInstagram = (req, res) => {
-  const url = `https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=${process.env.INSTAGRAM_CLIENT_ID}&redirect_uri=${process.env.REDIRECT_URI}&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish%2Cinstagram_business_manage_insights%2Cinstagram_manage_comments%2Cinstagram_basic`;
+  const scopes = [
+    "instagram_business_basic",
+    "instagram_business_content_publish",
+    "instagram_business_manage_messages",
+    "instagram_business_manage_comments",
+    "instagram_manage_comments",
+    "instagram_basic"
+  ].join("%2C"); // URL-encoded comma
+
+  const url = `https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=${process.env.INSTAGRAM_CLIENT_ID}&redirect_uri=${process.env.REDIRECT_URI}&response_type=code&scope=${scopes}`;
+
   res.redirect(url);
 };
 
